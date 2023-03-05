00a56b01dcbe4308615347f0c980fe432a11f09a
vinit-churi <vinitchuri@hotmail.com>
2023-03-05 13:54:08 +0530

    testing solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,31 @@
-import React from 'react'
+import React from "react";
 
-import '../styles/App.css';
-
+import "../styles/App.css";
 
 class App extends React.Component {
+    handleClick() {
+        this.setState({ count: this.state.count + 1 });
+    }
+    constructor(props) {
+        super(props);
+        this.state = { count: 0 };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    componentDidMount() {
+        console.log("mounted");
+    }
 
-  handleClick(){
-    this.setState({count: this.state.count+1})
-  }
-  constructor(props){
-    super(props)
-    this.state = {count:0}
-    this.handleClick = this.handleClick.bind(this);
-
-  }
-
-
-  
-  render() {
-    console.log('rendered')
-    return (
-      <div id="main">
-          <div id="count">{this.state.count}</div>
-          <button id="incr" onClick={this.handleClick}>Increment</button>
-      </div>
-    )
-  }
+    render() {
+        console.log("rendered");
+        return (
+            <div id="main">
+                <div id="count">{this.state.count}</div>
+                <button id="incr" onClick={this.handleClick}>
+                    Increment
+                </button>
+            </div>
+        );
+    }
 }
-
 
 export default App;
